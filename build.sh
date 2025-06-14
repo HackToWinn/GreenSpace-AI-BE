e6e580cd6216fc27a2a27dbd3cebaaeeca76904b
Alfian Ramadani <Alfianr792@gmail.com>
2025-06-14 23:12:42 +0800

    delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-# Build the canister
-dfx build backend
-
-# Buat folder tujuan jika belum ada
-mkdir -p ./api/src/canister/
-
-# Salin hasil build ke folder tertentu
-cp -r .dfx/local/canisters/ ./api/src/
